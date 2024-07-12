73815d8833ba3be3050f3291b0640f9757e8d681
Aigul Paramonova <aigul.paramanova@uds.app>
2024-07-12 21:34:17 +0300

    part 1 for review

---
+++ src/test/java/BurgerTest.java
@@ -0,0 +1,80 @@
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.MockitoJUnitRunner;
+import praktikum.Bun;
+import praktikum.Burger;
+import praktikum.Ingredient;
+import praktikum.IngredientType;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RunWith(MockitoJUnitRunner.class)
+public class BurgerTest {
+    @Mock
+    Bun bun;
+    @Mock
+    Ingredient sauceIngredient;
+    Burger burger = new Burger();
+    List<Ingredient> ingredients = new ArrayList<>();
+
+    @Test
+    public void setBunsTest() {
+        burger.setBuns(bun);
+        Assert.assertEquals(Constants.BUNS_CHOOSE, bun, burger.bun);
+    }
+
+    @Test
+    public void addIngredientTest() {
+        ingredients.add(sauceIngredient);
+        burger.addIngredient(sauceIngredient);
+        Assert.assertEquals(Constants.INGREDIENT_CHOOSE,ingredients, burger.ingredients);
+    }
+
+    @Test
+    public void removeIngredientTest() {
+        burger.addIngredient(sauceIngredient);
+        burger.removeIngredient(0);
+        Assert.assertEquals(Constants.INGREDIENT_CHOOSE_FOR_REMOVE,ingredients, burger.ingredients);
+    }
+
+    @Test
+    public void moveIngredientTest() {
+        burger.addIngredient(sauceIngredient);
+        burger.addIngredient(new Ingredient(IngredientType.FILLING, "cutlet", 100));
+        burger.moveIngredient(0, 1);
+        Assert.assertTrue(Constants.INGREDIENT_CHOOSE_FOR_MOVE,
+                burger.ingredients.indexOf(sauceIngredient) == 1);
+    }
+
+    @Test
+    public void getPriceTest(){
+        Mockito.when(bun.getPrice()).thenReturn(15.5f);
+        Mockito.when(sauceIngredient.getPrice()).thenReturn(10f);
+        burger.addIngredient(sauceIngredient);
+        burger.setBuns(bun);
+        Assert.assertTrue(Constants.INCORRECT_BURGER_PRICE,burger.getPrice() == 41f);
+    }
+
+    @Test
+    public void getReceiptTest() {
+        Mockito.when(bun.getName()).thenReturn("black bun");
+        Mockito.when(bun.getPrice()).thenReturn(15.5F);
+        Mockito.when(sauceIngredient.getPrice()).thenReturn(10f);
+        Mockito.when(sauceIngredient.getName()).thenReturn("hot sauce");
+        Mockito.when(sauceIngredient.getType()).thenReturn(IngredientType.SAUCE);
+        burger.setBuns(bun);
+        burger.addIngredient(sauceIngredient);
+        StringBuilder expected = new StringBuilder(String.format("(==== %s ====)%n", bun.getName()));
+        expected.append(String.format(
+                "= %s %s =%n",
+                sauceIngredient.getType().toString().toLowerCase(),
+                sauceIngredient.getName()));
+        expected.append(String.format("(==== %s ====)%n", bun.getName()));
+        expected.append(String.format("%nPrice: %f%n", burger.getPrice()));
+        Assert.assertEquals(Constants.INCORRECT_BURGER_RECEIPT,expected.toString(),burger.getReceipt());
+    }
+}
---
+++ src/test/java/Constants.java
@@ -0,0 +1,14 @@
+public class Constants {
+    public static final String INCORRECT_BUN_NAME = "Некорректное название булочки";
+    public static final String INCORRECT_BUN_PRICE = "Некорректная цена у булочки";
+    public static final String BUNS_CHOOSE = "Выберите булочку";
+    public static final String INGREDIENT_CHOOSE = "Выберите ингредиент";
+    public static final String INGREDIENT_CHOOSE_FOR_REMOVE = "Выберите ингредиент для удаления";
+    public static final String INGREDIENT_CHOOSE_FOR_MOVE = "Выберите ингредиент для перемещения";
+    public static final String INCORRECT_BURGER_PRICE = "Некорректная стоимость бургера";
+    public static final String INCORRECT_BURGER_RECEIPT = "Не тот бургер, бро";
+    public static final String BUNS_AVAILABLE = "Булочки недоступны";
+    public static final String INGREDIENT_AVAILABLE = "Ингредиенты недоступны";
+    public static final String INCORRECT_INGREDIENT_NAME = "Некорректное название ингредиента";
+    public static final String INCORRECT_INGREDIENT_TYPE = "Неорректный тип ингредиента";
+}
